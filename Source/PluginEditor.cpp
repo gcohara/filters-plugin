0987f242c054340a91b7e22b5dfb6c1182f77a2f
George O'Hara <georgeohara92@gmail.com>
2022-12-30 12:48:43 +0000

    initial GUI with 2 knobs

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,6 +16,17 @@ FiltersAudioProcessorEditor::FiltersAudioProcessorEditor (FiltersAudioProcessor&
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 200);
+    
+    // Set the look of the sliders (i.e as knobs).
+    // We can set by moving mouse left to right or up and down
+    cutoffFreq.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    resonance.setSliderStyle(juce::Slider::RotaryHorizontalVerticalDrag);
+    // Put the values underneath the knobs
+    cutoffFreq.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 90, 20);
+    resonance.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 90, 20);
+    // Make sliders/knobs visible
+    addAndMakeVisible(&cutoffFreq);
+    addAndMakeVisible(&resonance);
 }
 
 FiltersAudioProcessorEditor::~FiltersAudioProcessorEditor()
@@ -30,11 +41,13 @@ void FiltersAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("George's Lovely Filter", getLocalBounds(), juce::Justification::centredTop, 1);
 }
 
 void FiltersAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    cutoffFreq.setBounds(220, 20, 180, 180);
+    resonance.setBounds(0, 20, 180, 180);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,6 +28,9 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     FiltersAudioProcessor& audioProcessor;
+    
+    juce::Slider cutoffFreq;
+    juce::Slider resonance;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FiltersAudioProcessorEditor)
 };
